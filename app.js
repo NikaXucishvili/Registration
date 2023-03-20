07cfa60d6b3037f5fb8313ee6b8a0ccca681e412
Nika Xucishvili <103494069+NikaXucishvili@users.noreply.github.com>
2023-03-20 21:23:26 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-const title = 'Best reads of 2019';
-const author = 'Mario';
-const likes = 30;
+let ninjas = ['shaun', 'ryu', 'chun-li'];
 
+// ninjas[1] = 'ken';
+// console.log(ninjas[1]);
 
-let html = `
-    <h2>${title}</h2>;
-    <p>${author}</p>;
-`
-
-console.log(html);
