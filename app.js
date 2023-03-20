4643e52c5ab7f9a027d7268f8e381c8d37d99633
Nika Xucishvili <103494069+NikaXucishvili@users.noreply.github.com>
2023-03-20 21:25:40 +0400

    Update app.js

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ let ninjas = ['shaun', 'ryu', 'chun-li'];
 // ninjas[1] = 'ken';
 // console.log(ninjas[1]);
 
+let ages = [20,25,30,35];
+console.log(ages[2]);
+
+let names = ['Jason', 'Nick', 'Max'];
+names[2] = 'who';
+console.log(names[2]);
