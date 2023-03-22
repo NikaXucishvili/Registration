8acc911b21877ca8647947745d67899cc41cd90c
Nika Xucishvili <103494069+NikaXucishvili@users.noreply.github.com>
2023-03-22 14:31:16 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
-let ninjas = ['shaun', 'ryu', 'chun-li'];
+let names = ['max', 'derek', 'koger'];
 
-// ninjas[1] = 'ken';
-// console.log(ninjas[1]);
-
-// let ages = [20,25,30,35];
-// console.log(ages[2]);
-
-// let random = ['shaun', 'crystal', 30, 20];
-// console.log(random);
-
-// console.log(ninjas.length);
-
-// array methods
-
-// let result = ninjas.join('.');
-// let result = ninjas.indexOf('chun-li');
-// let result = ninjas.concat(['ken', 'crystal']);
-let result = ninjas.push('ken');
-result = ninjas.pop();
+let result = names.includes('max');
 console.log(result);
+
+let name = 'beneth';
+
+console.log(name == 'beneth');
+
+let score = '100';
+
+score = String(score);
+console.log(score + 1);
+console.log(typeof score);
