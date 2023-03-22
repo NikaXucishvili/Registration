551dd85f11fc0160d88df7f2cbbe6ca4646aeef9
Nika Xucishvili <103494069+NikaXucishvili@users.noreply.github.com>
2023-03-22 15:54:50 +0400

    Registration Form

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+const registrationForm = document.querySelector('#registration-form');
 
-for(let i = 0; i < 5; i++){
-    console.log('in loop:', i);
-}
+registrationForm.addEventListener('submit', (event) => {
+  event.preventDefault();
 
-console.log('loop finished');
+  /* Get form data and validate input values */
+  const username = document.querySelector('#username').value.trim();
+  const email = document.querySelector('#email').value.trim();
+  const password = document.querySelector('#password').value.trim();
+
+  if (!username || !email || !password) {
+    alert('Please fill in all fields');
+    return;
+  }
+  if(username == username || email == email || password == password){
+    alert('Form sumbitted successfully');
+  }
+  /* Submit the form data to the server */
+});
